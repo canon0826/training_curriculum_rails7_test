cf26c807e5a5486b253afbd2793bd2ecf4221fc0
canon0826 <147060718+canon0826@users.noreply.github.com>
2023-11-25 18:32:53 +0900

    Issue4〜6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,28 +1,49 @@
 class CalendarsController < ApplicationController
   def index
-    @week_days = getWeek # getWeekメソッドから曜日の配列を取得
+    get_week # get_weekメソッドから曜日の配列を取得
     @plan = Plan.new
   end
+
+  # 予定の保存
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+    redirect_to action: :index
+  else
+    # エラーが発生した場合、indexページを再描画してエラーメッセージを表示する
+    get_week
+    render :index
+  end
+end
+
+private
+
+def plan_params
+  params.require(:plan).permit(:date, :plan)
+end
   
-  private
-  
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
   
     @todays_date = Date.today
   
-    week_days = []
+    @week_days = []
+
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
   
     7.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+      day = @todays_date + x
       days = {
-        month: (@todays_date + x).month,
-        date: (@todays_date + x).day,
-        plans: [],
-        wday: wdays[(@todays_date + x).wday]
+        month: day.month,
+        date: day.day,
+        plans: today_plans,
+        wday: wdays[day.wday]
       }
-      week_days.push(days)
+      @week_days.push(days)
     end
-
-    week_days
   end
 end
